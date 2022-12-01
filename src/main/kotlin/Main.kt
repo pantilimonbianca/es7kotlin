be5776ea7565ab0299497f93d9030e183b331673
pantilimonbianca <pantilimonbianca00@gmail.com>
2022-12-01 18:40:12 +0100

    final commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+fun main() {
+    val nList = mutableListOf(9, 33, 1, 94, 54)
+   // println(numList)
+
+    val nString = nList.toString()
+   // println(numString)
+
+    val nStr = nString is String
+   // println(numStr)
+
+    nList.forEach{i -> println(i)}
+
+
+}
